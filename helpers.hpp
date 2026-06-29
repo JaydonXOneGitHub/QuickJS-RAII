6c40ffa7181b73223f337940a46de639e43aea21
JaydonXOneGitHub <jaydonyeany@outlook.com>
2026-06-29 14:07:42 -0400

    Update helpers.hpp

--- helpers.hpp
+++ helpers.hpp
@@ -1,4 +1,11 @@
 #define QUICKJS_FUNCTION_DECLARATION(name) \
+    JSValue name (JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv); \
+    QuickJS::Value name##Wrapped(QuickJS::Context& context, \
+        QuickJS::Value& thisValue, \
+        const std::vector<QuickJS::Value>& args \
+    );
+
+#define QUICKJS_STATIC_FUNCTION_DECLARATION(name) \
     static JSValue name (JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv); \
     static QuickJS::Value name##Wrapped(QuickJS::Context& context, \
         QuickJS::Value& thisValue, \
@@ -6,6 +13,18 @@
     );
 
 #define QUICKJS_FUNCTION_IMPLEMENTATION(name) \
+    JSValue name (JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) { \
+        QuickJS::Value thisValue = QuickJS::Value(ctx, this_val, false); \
+        QuickJS::Context context = QuickJS::Context(ctx); \
+        std::vector<QuickJS::Value> args = std::vector<QuickJS::Value>(); \
+        args.reserve(argc); \
+        for (int i = 0; i < argc; i++) { \
+            args.emplace_back(ctx, argv[i], false);  \
+        }\
+        return name##Wrapped(context, thisValue, args).getRawDuplicate(); \
+    }
+
+#define QUICKJS_STATIC_FUNCTION_IMPLEMENTATION(name) \
     static JSValue name (JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) { \
         QuickJS::Value thisValue = QuickJS::Value(ctx, this_val, false); \
         QuickJS::Context context = QuickJS::Context(ctx); \
@@ -17,7 +36,9 @@
         return name##Wrapped(context, thisValue, args).getRawDuplicate(); \
     }
 
-#define QUICKJS_METHOD_IMPLEMENTATION(type, name) \
+#define QUICKJS_STATIC_METHOD_DECLARATION(name) QUICKJS_FUNCTION_DECLARATION(name)
+
+#define QUICKJS_STATIC_METHOD_IMPLEMENTATION(type, name) \
     JSValue type :: name (JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) { \
         QuickJS::Value thisValue = QuickJS::Value(ctx, this_val, false); \
         QuickJS::Context context = QuickJS::Context(ctx); \
@@ -27,32 +48,4 @@
             args.emplace_back(ctx, argv[i], false);  \
         }\
         return type :: name##Wrapped(context, thisValue, args).getRawDuplicate(); \
-    }
-
-#define QUICKJS_MODULE_LOADER_DECLARATION(name) \
-    static JSModuleDef* name(JSContext *ctx, const char *module_name, void *opaque); \
-    static QuickJS::ModuleDef name##Wrapped(QuickJS::Context& context, \
-        const std::string& moduleName \
-    );
-
-#define QUICKJS_MODULE_LOADER_IMPLEMENTATION(name) \
-    static JSModuleDef* name(JSContext *ctx, const char *module_name, void *opaque) { \
-        QuickJS::Context context = QuickJS::Context(ctx); \
-        std::string moduleName(module_name ? module_name : ""); \
-        try { \
-            return name##Wrapped(context, moduleName).getRaw(); \
-        } catch (const std::exception& e) { \
-            return nullptr; \
-        } \
-    }
-
-#define QUICKJS_MODULE_LOADER_CLASS_IMPLEMENTATION(type, name) \
-    JSModuleDef* type :: name(JSContext *ctx, const char *module_name, void *opaque) { \
-        QuickJS::Context context = QuickJS::Context(ctx); \
-        std::string moduleName(module_name ? module_name : ""); \
-        try { \
-            return type :: name##Wrapped(context, moduleName).getRaw(); \
-        } catch (const std::exception& e) { \
-            return nullptr; \
-        } \
     }
